f174269f5e8554ee94ad869ca730662fbc332a78
Anuj Singal <anujsingal203@gmail.com>
2026-05-04 11:20:36 +0530

    Basic notification APIs implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,9 @@ app.use(express.json());
 const logger = require("./middleware/logger");
 app.use(logger);
 
+app.use("/api/notifications", require("./routes/notification.routes"));
+
+
 app.get("/", (req, res) => {
   res.send("Server running");
 });
